fae0f9719174f9e1fff3519f90053038d35379dc
Sahil <sohil170246@gmail.com>
2022-06-27 14:44:38 +0530

    app designed

--- index.js
+++ index.js
@@ -3,52 +3,174 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-
+const moment = require('moment');
+const router = express.Router();
 const cors = require('cors');
-
+const shortid = require('shortid');
 const mongoose = require('mongoose');
+const e = require('express');
+// const {Schema} = mongoose;
 mongoose.connect("mongodb+srv://sahilsaini110:sTweqfvmhk2yXfOY@cluster0.qm4f15a.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://localhost/exercise-track' );
 
-require('./models');
+// require('./models');
+// const User = mongoose.model('User');
+// const Exercise = mongoose.model('Exercise');
 
+
+// const api = require('./routes/exercise');
+// const api2 = require('./routes/index');
+// app.use('/api', api);
+// app.use('/api', api2);
+
+// schema1 
+const UserSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: shortid.generate
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    }
+})
+
+const UserInfo = mongoose.model('User', UserSchema);
+
+//  schema2
+const ExerciseSchema = new mongoose.Schema({
+  description: {
+      type: String,
+      required: true,
+      maxlength: [25, 'Description too long, not greater than 25']
+  },
+  duration: {
+      type: Number,
+      required: true,
+      min: [1, 'Duration too short, at least 1 minute']
+  },
+  date: {
+      type: Date, 
+      default: Date.now
+  },
+  userId: {
+      type: String,
+  }
+})
+
+const ExerciseInfo = mongoose.model('Exercise', ExerciseSchema);
+
+// schema3
+const logSchema = new mongoose.Schema({
+  "username":  String,
+  "count": Number,
+  "log": Array
+});
+
+LogInfo = mongoose.model('log', logSchema);
+
+// middleware
 app.use(cors());
-app.use(logger('dev'));
-
+// app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+// // Not found middleware
+// app.use((req, res, next) => {
+//   return next({status: 404, message: 'not found'})
+// });
 
-const api = require('./routes');
-app.use('/api', api);
+// // Error Handling middleware
+// app.use((err, req, res, next) => {
+//   let errCode, errMessage
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-});
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
+//   if (err.errors) {
+//     // mongoose validation error
+//     errCode = 400 // bad request
+//     const keys = Object.keys(err.errors)
+//     // report the first validation error
+//     errMessage = err.errors[keys[0]].message
+//   } else {
+//     // generic or custom error
+//     errCode = err.status || 500
+//     errMessage = err.message || 'Internal Server Error'
+//   }
+//   res.status(errCode).type('txt')
+//     .send(errMessage)
+// });
+ 
+app.post("/api/users", (req, res) => {
+  console.log("ji");
+ UserInfo.find({"username" : req.body.username}, (err, userData) => {
+  if (err){
+    console.log(err)
   } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+    if (userData.length === 0){
+      const test = new UserInfo({
+        "_id": req.body.id,
+        "username": req.body.username
+      })    
+      test.save((err, data) => {
+        if (err){
+          console.log(err)
+        } else {
+          res.json({
+            "_id":data.id,
+            "username": data.username
+          })
+        }
+      })
+    } else {
+      res.send ("user hai phle se")
+    }
   }
-  res.status(errCode).type('txt')
-    .send(errMessage)
+ })
 });
+
+app.post('/api/users/:_id/exercises', (req, res) =>{
+  let idJson = { "id": req.params._id};
+  let checkedDate = new Date(req.body.date);
+  let idToCheck = idJson.id;
+  let noDateHandler = () => {
+    if (checkedDate instanceof Date && !isNaN(checkedDate)){
+      return checkedDate
+    } else {
+      checkedDate = new Date();
+    }
+  }
+
+  UserInfo.findById(idToCheck, (err, data) =>{
+    noDateHandler(checkedDate);
+
+    if(err) {
+      console.log("err", err);
+    } else {
+      const test = new ExerciseInfo({
+        "username": data.username,
+        "description": req.body.description,
+        "duration": req.body.duration,
+        "date": checkedDate.toDateString()
+      })
+      test.save((err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("saved exercise acchese");
+          res.json({
+            "_id": idToCheck,
+            "username": data.username,
+            "description": data.description,
+            "duration": data.duration,
+            "date": data.date.toDateString()
+          })
+        }
+      })
+    }
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
