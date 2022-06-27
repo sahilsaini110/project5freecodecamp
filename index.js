a6d7d0f9338a29acf730226b5502941d634c4e6d
Sahil <sohil170246@gmail.com>
2022-06-27 15:20:52 +0530

    app designed

--- index.js
+++ index.js
@@ -172,6 +172,104 @@ app.post('/api/users/:_id/exercises', (req, res) =>{
   })
 })
 
+
+app.get('/api/users/:_id/logs', (req, res, next) => {
+  let { userId, from, to, limit } = req.query;
+  let id = req.params._id;
+  from = moment(from, 'YYYY-MM-DD').isValid() ? moment(from, 'YYYY-MM-DD') : 0;
+  to = moment(to, 'YYYY-MM-DD').isValid() ? moment(to, 'YYYY-MM-DD') : moment().add(1000000000000);
+  UserInfo.findById(id).then(user => {
+      if (!user) throw new Error('Unknown user with _id');
+      ExerciseInfo.find({ userId })
+          .where('date').gte(from).lte(to)
+          .limit(+limit).exec()
+          .then(log => res.status(200).send({
+              _id: userId,
+              username: user.username,
+              count: log.length,
+              log: log.map(o => ({
+                  description: o.description,
+                  duration: o.duration,
+                  date: moment(o).format('ddd MMMM DD YYYY')
+              }))
+          }))
+  })
+      .catch(err => {
+          console.log(err);
+          res.status(500).send(err.message);
+      })
+})
+
+
+// app.get('/api/users/:_id/logs', (req, res) => {
+//   const {from, to , limit} = req.query;
+//   let idJson = {"id": req.params._id};
+//   let idToCheck = idJson.id;
+
+//   UserInfo.findById(idToCheck, (err, data) =>{
+//     var query = {
+//       username: data.username
+//     }
+//     if(from !== undefined && to === undefined){
+//       query.date ={$gte: new Date(from)}
+//     } else if (to !== undefined && from === undefined) {
+//       query.date = {$lte: new Date(to)}
+//     } else if (from !== undefined && to !== undefined){
+//       query.date = {$gte: new Date(from), $lte: new Date(to)}
+//     }
+
+//     let limitChecker = (limit) =>{
+//       let maxLimit = 100;
+//       if(limit) {
+//         return limit;
+//       } else {
+//         return maxLimit
+//       }
+//     }
+
+//     if(err) {
+//       console.log(err)
+//     } else{
+//       ExerciseInfo.find((query), null, {limit: limitChecker(+limit)}, (err, data) => {
+//         let loggedArray = [];
+//         if (err){
+//           console.log(err)
+//         } else {
+//           let documents = docs;
+//           let loggedArray = documents.map((item) => {
+//             return {
+//               "description": item.description,
+//               "duration": item.duration,
+//               "log": item.date.toDateString()
+//             }
+//           })
+
+//           const test = new LogInfo({
+//             "username": data.username,
+//             "count": loggedArray.length,
+//             "log": loggedArray
+//           })
+//           test.save((err, data) => {
+//             if(err) {
+//               console.log(err)
+//             }else {
+//               console.log("save hpogya");
+//               res.json({
+//                 "_id": idToCheck,
+//                 "username": data.username,
+//                 "count": data.count,
+//                 "log": loggedArray
+//               })
+//             }
+//           })
+//         }
+//       })
+//     }
+//   })
+// })
+
+
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 });
